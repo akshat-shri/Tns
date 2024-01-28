cca9e410ea339704e00802529f29b6caf724c590
Akshat Shrivastava <88078409+akshat-shri@users.noreply.github.com>
2024-01-28 15:51:19 +0530

    Add files via upload

---
+++ javaoops/src/com/arya/inheritance/single/classes/Parent.java
@@ -0,0 +1,24 @@
+package com.arya.inheritance.single.classes;
+
+public class Parent {
+protected String lastName;
+	
+	public Parent() {
+		this.lastName=null;
+	}
+	
+	public Parent(String lastName) {
+		this.lastName=lastName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+}
